be907ca4ad32ad32d37d764aad981317b0f50864
arun276-karthik <arunkarthik.periyaswamy@ideas2it.com>
2021-05-06 02:16:21 +0530

    right side center completed

--- todaList/js/app.js
+++ todaList/js/app.js
@@ -1,89 +1,172 @@
-let categoryList = [{
-        id: '1',
-        title: 'My Day',
-        icon: "fa fa-sun-o"
-    },
-    {
-        id: '2',
-        title: 'Important',
-        icon: "fa fa-star-o"
-    },
-    {
-        id: '3',
-        title: 'Planned',
-        icon: "fa fa-calendar"
-    },
-    {
-        id: '4',
-        title: 'Assinged to you',
-        icon: "fa fa-user-o"
+(() => {
+    let categoryList = [{
+            id: "1",
+            title: 'My Day',
+            icon: "fa fa-sun-o"
+        },
+        {
+            id: "2",
+            title: 'Important',
+            icon: "fa fa-star-o"
+        },
+        {
+            id: "3",
+            title: 'Planned',
+            icon: "fa fa-calendar"
+        },
+        {
+            id: "4",
+            title: 'Assinged to you',
+            icon: "fa fa-user-o"
+        }
+    ];
+
+    let taskList = [];
+
+    //variables 
+    const button = document.getElementById("add-btn");
+    const input = document.getElementById("add-task");
+    const leftBlock = document.getElementById("left-block");
+    const rightHeading = document.getElementById("right-heading");
+    const toggleButton = document.getElementById("toggle-button");
+    const subTask = document.getElementById("sub-task");
+    const subTaskBtn = document.getElementById("add-task-btn");
+    const subTaskBlock = document.getElementById("sub-task-block");
+    const leftContainer = document.getElementById("left-container");
+
+    let categoryId;
+
+    //CategoryList objects
+    categoryList.forEach(listFunction);
+
+    function listFunction(category) {
+
+        const newElement = document.createElement('div');
+        newElement.innerHTML += " <i class='" + category.icon + "'></i><span>" + category.title + "</span>";
+        newElement.setAttribute("class", "category");
+        //newElement.setAttribute("id", "categoryIId");
+        newElement.setAttribute("id", category.id);
+        leftBlock.append(newElement);
+        document.querySelectorAll(".category").forEach(categorySelect => {
+            categorySelect.addEventListener("click", function(evt) {
+                categoryId = evt.target.id;
+                for (let i = 0; i < categoryList.length; i++) {
+                    if (categoryList[i].id == categoryId) {
+                        rightHeading.innerHTML = "<h3>" + categoryList[i].title + "</h3>";
+                    }
+                }
+            });
+        });
     }
-];
-const button = document.getElementById("add-btn");
-const input = document.getElementById("add-task");
-const taskContainer = document.getElementById("left-block");
 
-categoryList.forEach(listFunction);
+    // let categoryId = document.querySelectorAll(".category").forEach(categorySelect => {
+    //     categorySelect.addEventListener("click", function(evt) {
+    //         console.log(evt.target.id);
+    //         console.log(categorySelect);
+    //     });
+    // });
 
-function listFunction(item) {
-    taskContainer.innerHTML += " <div class='category'> <i class='" +
-        item.icon + "'></i><span>" + item.title + "</span></div>";
-}
+    //console.log(categoryId);
+    //alert(categoryId);
 
+    //Toggle function in left side
+    toggleButton.addEventListener("click", function() {
+        if (leftContainer.style.width === "25%") {
+            leftContainer.style.width = "3%"
+        } else {
+            leftContainer.style.width = "25%"
+        }
+        //this.classList.toggle("left-container-class");
+    });
 
-function addTask() {
-    //Taking user Input
-    const inputValue = input.value;
+    //Task to render into the category List
+    function addTask() {
+        const inputValue = input.value;
+        if (inputValue == "") return;
 
-    //Creating new Element with value
-    const newElement = document.createElement('div');
-    newElement.innerHTML = "<i class='fa fa-list'></i><span >" + inputValue + "</span>";
+        let listData = {
+            id: Math.round(Math.random() * 10000000).toString(),
+            title: inputValue,
+            icon: 'fa fa-list'
+        }
+        categoryList.push(listData);
+        leftBlock.innerHTML = "";
+        categoryList.forEach(listFunction);
+        input.value = "";
+    }
 
-    newElement.className = 'taskAddition';
+    function handleEnter(event) {
+        if (event.keyCode === 13) addTask();
+    }
 
-    //Appending to container
-    taskContainer.append(newElement);
-}
+    button.addEventListener("click", addTask);
+    input.addEventListener("keyup", handleEnter);
 
-//addNewList();
+    // Adding right Side Task 
+    function addSubTask() {
+        const inputValue = subTask.value;
+        if (inputValue == "") return;
 
-// function addNewList() {
-//     var newElement = document.createElement('div');
-//     newElement.innerHTML = "<i class='fa fa-plus'></i><input type='text' class='add-task' placeholder='New list' >" +
-//         "<button class='btn'>click</button>";
-//     newElement.className = 'newTask';
-//     newElement.id = '5';
-//     const taskvalue = add-task.value;
-//     //console.log(taskvalue);
-//     // const leftBlock = document.getElementById("left-block");
-//     // leftBlock.append(newElement);
-//     document.getElementById("left-block").appendChild(newElement);
-// }
+        let taskListData = {
+            id: Math.round(Math.random() * 1000000).toString(),
+            title: inputValue,
+            icon: 'fa fa-list',
+            categoryId: categoryId
+        }
 
-function handleEnter(event) {
-    if (event.keyCode === 13) addTask();
-}
+        taskList.push(taskListData);
 
-button.addEventListener("click", addTask);
-input.addEventListener("keyup", handleEnter);
+        const newElement = document.createElement('div');
+        newElement.innerHTML = "<i class='fa fa-circle-thin'></i><span >" + inputValue + "</span>";
 
-// const addButton = document.getElementsByClassName("add-button");
-// const addNote = document.getElementsByClassName("add-Note");
-// const leftBlock = document.getElementById("left-block");
+        newElement.setAttribute("class", "subTaskAddition");
+        newElement.setAttribute("id", "subTaskAdd");
+        newElement.addEventListener("click", taskClick);
+        subTaskBlock.append(newElement);
 
-// function addTask() {
-//     const addTask = addNote.value;
+        document.querySelectorAll(".category").forEach(categorySelect => {
+            categorySelect.addEventListener("click", function(evt) {
+                categoryId = evt.target.id;
+                subTaskBlock.innerHTML = "";
+                for (let i = 0; i < taskList.length; i++) {
+                    if (categoryId === taskList[i].categoryId) {
+                        const newElement = document.createElement('div');
+                        newElement.innerHTML = "<i class='fa fa-circle-thin'></i><span >" + taskList[i].title + "</span>";
 
-//     const newElement = document.createElement("div");
-//     newElement.innerText = addTask;
+                        newElement.setAttribute("class", "subTaskAddition");
+                        newElement.setAttribute("id", "subTaskAdd");
+                        newElement.addEventListener("click", taskClick);
+                        subTaskBlock.append(newElement);
+                    }
+                }
+            });
+        });
 
+        subTask.value = "";
+    }
 
-//     leftBlock.append(newElement);
-// }
+    function handleEnterkey(event) {
+        if (event.keyCode === 13) addSubTask();
+    }
 
-// function handleEnter(e) {
-//     if (e.keyCode === 13) addTask();
-// }
+    // function iconChange(x) {
+    //     x.classList.toggle("fa-check-circle-o")
+    // }
 
-// addButton.addEventListener("click", addTask);
-// addNote.addEventListener("keyup", handleEnter);
+    subTaskBtn.addEventListener("click", addSubTask);
+    subTask.addEventListener("keyup", handleEnterkey);
+
+    //document.getElementById("subTaskAdd").addEventListener("click", taskClick);
+
+    function taskClick() {
+        this.classList.toggle("taskCompleted");
+    }
+
+    // function backGroundLines() {
+    //     var elem = document.createElement("div");
+    //     elem.setAttribute("class", "backgroundLines");
+    //     document.body.appendChild(elem);
+    // }
+
+    init();
+})();
